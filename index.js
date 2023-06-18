b1f6ecf1611b3fa74fb7d88f7bb87cb66b396e44
likhithabandaru2003 <likhithabandaru2003@gmail.com>
2023-06-18 12:28:02 +0530

    updated

--- index.js
+++ index.js
@@ -3,33 +3,26 @@ const passport = require('passport');
 const session =require('express-session');
 const path=require('path');
 const app=express();
-const mime = require('mime');
+const mime = require('mime-types');
 require('./auth');
-require('./weather');
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname,"client")));
 app.use(express.static(path.join(__dirname,"weather")));
 app.get('/auth/style.css', function(req, res) {
     const filePath = __dirname+'/weather/style.css'; 
-    const mimeType = mime.lookup(filePath);
   
-    if (mimeType) {
-      res.setHeader('Content-Type', mimeType);
+    
+      res.setHeader('Content-Type', 'text/css');
       res.sendFile(filePath);
-    } else {
-      res.status(404).send('File not found');
-    }
+    
   });
   app.get('/auth/script.js', function(req, res) {
     const filePath = __dirname+'/weather/script.js'; 
-    const mimeType = mime.lookup(filePath);
-  
-    if (mimeType) {
-      res.setHeader('Content-Type', mimeType);
+   
+      res.setHeader('Content-Type', 'text/javascript');
       res.sendFile(filePath);
-    } else {
-      res.status(404).send('File not found');
-    }
+    
   });
 function isLoggedIn(req,res,next){
     req.user ? next() : res.sendStatus(401);
@@ -64,6 +57,98 @@ app.get('/auth/google/failure',(req,res)=>{
 app.get('/auth/protected',isLoggedIn,(req,res)=>{
     res.sendFile(__dirname+"/weather/index.html")
 });
+
+app.get("/latlon", async (req, res) => {
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+  const data = await getWeatherLatLon(lat, lon);
+  res.json(data);
+});
+app.get("/weather", async (req, res) => {
+  const city = req.query.city;
+  const data = await getWeather(city);
+  res.json(data);
+});
+
+async function getWeatherLatLon(lat, lon) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9c25d71309f799febed483efc24e0b5a`
+    );
+
+    const weatherData = await response.json();
+    console.log(weatherData);
+    let tempicon;
+    let id = weatherData.weather[0].id;
+    console.log("id", id);
+    if (id >= 200 && id < 300) {
+      tempicon = "thunderstorm.png";
+    } else if (id >= 300 && id < 400) {
+      tempicon = "clouds.png";
+    } else if (id >= 500 && id < 600) {
+      tempicon = "rains.png";
+    } else if (id >= 600 && id < 700) {
+      tempicon = "snow.png";
+    } else if (id >= 700 && id < 800) {
+      tempicon = "clouds.png";
+    } else if (id >= 800) {
+      tempicon = "clouds.png";
+    }
+    const data = {
+      iconfile: `https://raw.githubusercontent.com/likhithabandaru2003/login-weather-app/main/images/${tempicon}`,
+      tempvalue: weatherData.main.feels_like,
+      humidity:weatherData.main.humidity,
+      pressure:weatherData.main.pressure,
+      city: weatherData.name,
+      id: weatherData.weather[0].id,
+      climate: weatherData.weather[0].description,
+    };
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getWeather(city) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9c25d71309f799febed483efc24e0b5a`
+    );
+
+    const weatherData = await response.json();
+
+    let tempicon;
+    let id = weatherData.weather[0].id;
+    console.log("id", id);
+    if (id >= 200 && id < 300) {
+      tempicon = "thunderstorm.png";
+    } else if (id >= 300 && id < 400) {
+      tempicon = "clouds.png";
+    } else if (id >= 500 && id < 600) {
+      tempicon = "rains.png";
+    } else if (id >= 600 && id < 700) {
+      tempicon = "snow.png";
+    } else if (id >= 700 && id < 800) {
+      tempicon = "clouds.png";
+    } else if (id >= 800) {
+      tempicon = "clouds.png";
+    }
+    const data = {
+      iconfile: `https://raw.githubusercontent.com/likhithabandaru2003login-weather-app/main/images/clouds.png/${tempicon}`,
+      tempvalue: weatherData.main.feels_like,
+      city: weatherData.name,
+      id: weatherData.weather[0].id,
+      humidity:weatherData.main.humidity,
+      pressure:weatherData.main.pressure,
+      climate: weatherData.weather[0].description,
+    };
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
 
 app.listen(5000,()=>{
     console.log('listening at port 5000');
